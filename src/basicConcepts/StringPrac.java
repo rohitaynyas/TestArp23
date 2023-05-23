18fe99e0c8f363e2cc9aaf0fb12f4db1dc243854
rohitaynyas <rohitaynyas@gmail.com>
2023-05-23 09:01:16 +0530

    chnages

--- src/basicConcepts/StringPrac.java
+++ src/basicConcepts/StringPrac.java
@@ -4,7 +4,8 @@ public class StringPrac {
 	
 	public static void main(String[] args) {
 		System.out.println("I am in main method...branch A");
-		System.out.println("Creating cofilt scenarion");	
+		System.out.println("Creating cofilt scenarion");
+		System.out.println("Creating cofilt scenarion");
 	}
 }
 
